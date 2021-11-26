b2d89dd49f239fac76bd1a7d718a0dc6e029820e
unknown <dusdjeks@naver.com>
2021-11-26 11:21:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import * as React from "react";
-import {Admin} from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import {Admin, Resource} from 'react-admin';
+import {PostList, PostEdit, PostCreate} from './posts';
+import {UserList} from './users';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
+import authProvider from "./authProvider";
+import dataProvider from "./dataProvider";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App=()=><Admin dataProvider={dataProvider}/>;
+
+const App=()=>(
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+        <Resource name="users" list={UserList} icon={UserIcon}/>
+    </Admin>
+);
 
 export default App;
